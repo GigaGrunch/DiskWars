3c8cda41fdc8dc8450b8016d46ca6e34996fbd2b
GigaGrunch <gigagrunch@gmail.com>
2020-11-02 18:15:58 +0100

    add main menu to select loner/host/guest

---
+++ Assets/Code/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace DiskWars
+{
+    public class MainMenu : MonoBehaviour
+    {
+        [SerializeField] private Button _singleplayerButton;
+        [SerializeField] private Button _hostButton;
+        [SerializeField] private Button _joinButton;
+
+        public void Start()
+        {
+            _singleplayerButton.onClick.AddListener(StartSingleplayer);
+            _hostButton.onClick.AddListener(HostGame);
+            _joinButton.onClick.AddListener(JoinGame);
+        }
+
+        private void StartSingleplayer()
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+
+        private void HostGame()
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+
+        private void JoinGame()
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+}
